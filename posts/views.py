1b9f62123593627b667505c59b12bfaca803b466
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-04-03 19:39:20 +0000

    Add docstrings to post views

--- posts/views.py
+++ posts/views.py
@@ -5,6 +5,10 @@ from django_rest_api.permissions import IsOwnerOrReadOnly
 
 
 class PostList(generics.ListCreateAPIView):
+    """
+    List posts or create a post if logged in
+    The perform_create method associates the post with the logged in user
+    """
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.all()
@@ -14,6 +18,9 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a post and edit or delete it if you own it
+    """
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.all()
