a68f4a1a0c09178a2c5feb441f66e1d500b74818
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-04-04 20:29:43 +0000

    Add further filtering to profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Profile
 from .serializers import ProfileSerializer
 from django_rest_api.permissions import IsOwnerOrReadOnly
@@ -18,7 +19,11 @@ class ProfileList(generics.ListAPIView):
         following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        'owner__following__followed__profile',
     ]
     ordering_fields = [
         'posts_count',
